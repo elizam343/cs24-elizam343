1e1c36fc32aac82524a7dec9377e647547bb2b96
Eliseo Zamora Aguilar <eliseo@ucsb.edu>
2023-07-27 22:29:25 -0700

    code

--- calculator/calc.cpp
+++ calculator/calc.cpp
@@ -1,3 +1,4 @@
+// This is calc.cpp
 #include "Stack.h"
 
 #include <iostream>
@@ -13,97 +14,85 @@ double apply_operator(double a, double b, const std::string& op) {
     if (op == "-") return a - b;
     if (op == "*") return a * b;
     if (op == "/") {
-        if (b == 0) {
-            throw std::invalid_argument("Division by zero");
+        if(b == 0.0){
+            throw std::runtime_error("Division by zero");
         }
         return a / b;
     }
     if (op == "%") {
-        if (b == 0) {
-            throw std::invalid_argument("Modulo by zero");
+        if(b == 0.0){
+            throw std::runtime_error("Division by zero");
         }
         return std::fmod(a, b);
     }
     if (op == "^") return std::pow(a, b);
-    if (op == "~") return -a; // Negates the number
-    return 0.0; // Operator not supported, but this should never happen in valid input.
+    throw std::runtime_error("Unknown token");
 }
-
 
 int main() {
     Stack* mathstack = Stack::create();
     std::string line;
 
     while (std::getline(std::cin, line)) {
-        if(line.empty()) {
-            std::cout << "No expression." << std::endl;
-            continue;
-        }
-
         std::istringstream iss(line);
         std::string token;
         bool error = false;
 
         while (iss >> token) {
-            try {
-                if (is_operator(token)) {
+            if (is_operator(token)) {
+                if (mathstack->is_empty()) {
+                    std::cout << "Not enough operands." << std::endl;
+                    error = true;
+                    break;
+                }
+
+                double b = mathstack->pop();
+
+                if (token == "~") {
+                    mathstack->push(-b);
+                } else {
                     if (mathstack->is_empty()) {
                         std::cout << "Not enough operands." << std::endl;
                         error = true;
                         break;
                     }
 
-                    double b = mathstack->pop();
-                    double a = 0.0;
-
-                    if (token != "~") {
-                        if (mathstack->is_empty()) {
-                            std::cout << "Not enough operands." << std::endl;
-                            error = true;
-                            break;
-                        }
-
-                        a = mathstack->pop();
+                    double a = mathstack->pop();
+                    try {
+                        double result = apply_operator(a, b, token);
+                        mathstack->push(result);
+                    } catch (std::runtime_error& e) {
+                        std::cout << e.what() << std::endl;
+                        error = true;
+                        break;
                     }
-
-                    double result = apply_operator(a, b, token);
-                    mathstack->push(result);
-                } else {
+                }
+            } else {
+                try {
                     double value = std::stod(token);
                     mathstack->push(value);
+                } catch (std::invalid_argument&) {
+                    std::cout << "Unknown token." << std::endl;
+                    error = true;
+                    break;
                 }
-            } catch (std::invalid_argument& e) {
-                std::cout << e.what() << std::endl;
-                error = true;
-                break;
-            } catch (std::out_of_range& e) {
-                std::cout << "Unknown token." << std::endl;
-                error = true;
-                break;
             }
         }
 
-        if (error) {
-            while (!mathstack->is_empty()) mathstack->pop();
+        if (error || mathstack->is_empty()) {
+            if (mathstack->is_empty()) {
+                std::cout << "No expression." << std::endl;
+            } else {
+                std::cout << "Too many operands." << std::endl;
+            }
             continue;
         }
 
-        if (mathstack->is_empty()) {
-            std::cout << "No expression." << std::endl;
-            continue;
-        }
-
-        double result = mathstack->pop();
-        if (!mathstack->is_empty()) {
-            std::cout << "Too many operands." << std::endl;
-            while (!mathstack->is_empty()) mathstack->pop();
-            continue;
-        }
-
+        double result = mathstack->top();
         std::cout << "= " << result << std::endl;
+        mathstack->clear();
     }
 
     delete mathstack;  // don't forget to delete the pointer
     return 0;
 }
-
