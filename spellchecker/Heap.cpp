06ecd64cceaeaa4d529b974855f1daef17dceb8d
Eliseo Zamora Aguilar <eliseo@ucsb.edu>
2023-08-12 22:29:09 -0700

    code

--- spellchecker/Heap.cpp
+++ spellchecker/Heap.cpp
@@ -114,7 +114,7 @@ Heap::Entry Heap::pushpop(const std::string& value, float score) {
     // If the heap is empty or the given score is less than the top score, 
     // simply return the given value and score without altering the heap.
     if (mCount == 0 || score < mData[0].score) {
-        return {value, score};
+        throw std::underflow_error("Heap is empty");
     }
 
     // If the score is greater than or equal to the top score, 
