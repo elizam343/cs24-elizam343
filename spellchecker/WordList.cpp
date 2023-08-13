892846af23775d8295edff49f39f7196090e4a56
Eliseo Zamora Aguilar <eliseo@ucsb.edu>
2023-08-12 23:59:53 -0700

    code

--- spellchecker/WordList.cpp
+++ spellchecker/WordList.cpp
@@ -13,46 +13,39 @@ WordList::WordList(std::istream& stream) {
 Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const {
     Heap heap(maxcount);
 
-    for (const auto& word : mWords) {
-        if (word.length() != points.size()) {
-            continue;  // Skip words of different lengths
+    float tot = 0.0;
+    for (std::string str : mWords) {
+        if (points.size() != str.length()) {
+            continue;
         }
 
-        float totalScore = 0.0;
-
-        for (size_t i = 0; i < word.length(); ++i) {
-            char c = word[i];  // Removed tolower()
-
-            float score;
-
-            if (c >= 'a' && c <= 'z') {
-                Point charPoint = QWERTY[c - 'a'];
-
-                float dx = charPoint.x - points[i].x;
-                float dy = charPoint.y - points[i].y;
-                float distance = sqrt(dx * dx + dy * dy);
-
-                score = 1 / (10 * distance * distance + 1);
-            } else {
-                // Handle non-lowercase alphabetic characters or assign default score
-                // Here, you might want to set a default score or add some specific handling
-                // for characters like 'I'. For now, let's assign a default score.
-                score = 0.001;  // Assigning a default low score
+        for (size_t i = 0; i < str.length(); i++) {
+            if (str[i] < 'a' || str[i] > 'z') {
+                // Handle non-lowercase characters
+                // Assuming you want to continue and ignore this word
+                tot = 0.0;
+                break;
             }
-            
-            totalScore += score;
+
+            Point charStr = QWERTY[str[i] - 'a'];
+            float x_distance = charStr.x - points[i].x;
+            float y_distance = charStr.y - points[i].y;
+            float distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+            float score = 1 / (10 * (distance * distance) + 1);
+            tot += score;
         }
 
-        float averageScore = totalScore / word.length();
+        float avg = tot / str.length();
 
-        if (averageScore > cutoff) {
+        if (avg > cutoff) {
             if (heap.count() < maxcount) {
-                heap.push(word, averageScore);
-            } else if (averageScore > heap.top().score) {
-                heap.pushpop(word, averageScore);
+                heap.push(str, avg);
+            } else if (avg > heap.top().score) {
+                heap.pushpop(str, avg);
             }
         }
+        tot = 0.0;
     }
-
     return heap;
 }
+
