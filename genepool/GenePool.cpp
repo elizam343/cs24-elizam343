d834a2e034adaf5574444b5c8373998b2aea0485
Eliseo Zamora Aguilar <eliseo@ucsb.edu>
2023-09-01 22:36:56 -0700

    code

--- genepool/GenePool.cpp
+++ genepool/GenePool.cpp
@@ -3,73 +3,57 @@
 #include "GenePool.h"
 
 GenePool::GenePool(std::istream& stream) {
-    std::map<std::string, std::pair<std::string, std::string>> parentNames;  // to store mother and father names for each person
-
     std::string line;
+
     while (std::getline(stream, line)) {
         std::istringstream iss(line);
-        std::string name, genderStr, motherName, fatherName;
+        std::string name, genderStr;
         Gender gender;
 
-        std::getline(iss, name, '\t');
-        std::getline(iss, genderStr, '\t');
-        std::getline(iss, motherName, '\t');
-        std::getline(iss, fatherName, '\t');
+        // Assuming the input format is: NAME GENDER
+        if (!(iss >> name >> genderStr)) {
+            // Error in parsing, handle or continue to next line
+            continue;
+        }
 
         if (genderStr == "MALE") {
             gender = Gender::MALE;
         } else if (genderStr == "FEMALE") {
             gender = Gender::FEMALE;
         } else {
-            // Error handling or skip
+            // Invalid gender, handle or continue to next line
             continue;
         }
 
-        Person* person = new Person(name, gender);  // Assuming Person has this constructor
+        Person* person = new Person(name, gender);
         mPeople[name] = person;
-        
-        parentNames[name] = {motherName, fatherName};  // store mother and father names for linkage later
     }
 
-    // Link parents after populating all persons
-    for (const auto& [name, person] : mPeople) {
-        const auto& [motherName, fatherName] = parentNames[name];  // extract mother and father names
-
-        if (mPeople.find(motherName) != mPeople.end()) {
-            person->setMother(mPeople[motherName]);  // set mother
-        }
-        if (mPeople.find(fatherName) != mPeople.end()) {
-            person->setFather(mPeople[fatherName]);  // set father
-        }
-    }
+    // After all people are created, you can loop again to assign relationships if needed.
+    // Example: If you have mother and father names in the file, you would loop through again 
+    // and use `find` to locate and assign the mother and father to each person.
 }
 
 GenePool::~GenePool() {
-    for (auto& [name, person] : mPeople) {
-        delete person;
+    for (auto& entry : mPeople) {
+        delete entry.second;
     }
 }
 
 std::set<Person*> GenePool::everyone() const {
-    std::set<Person*> result;
-    for (const auto& [name, person] : mPeople) {
-        result.insert(person);
+    std::set<Person*> allPeople;
+    for (const auto& pair : mPeople) {
+        allPeople.insert(pair.second);
     }
-    return result;
+    return allPeople;
 }
 
 Person* GenePool::find(const std::string& name) const {
-    if (mPeople.find(name) != mPeople.end()) {
-        return mPeople.at(name);
+    auto it = mPeople.find(name);
+    if (it != mPeople.end()) {
+        return it->second;
     }
     return nullptr;
-}
-
-
-GenePool::~GenePool() {
-    for (auto& [name, person] : mPeople) {
-        delete person;
-    }
 }
 
 std::set<Person*> GenePool::everyone() const {
